6370f92ce225dde10947dc6dc88c3eb15de473f4
NukaGrizz <nukagrizz@gmail.com>
2022-05-03 03:07:19 -0400

    complete Calculator class, complete CalculatorTest, compete AppCalculator

--- CalculateObject/src/main/java/com/company/Calculator.java
+++ CalculateObject/src/main/java/com/company/Calculator.java
@@ -2,7 +2,20 @@ package com.company;
 
 public class Calculator {
 
+    public int add(int one, int two) { return one + two; }
 
+    public double add(double one, double two) { return one + two; }
 
+    public int subtract(int one, int two) { return one - two; }
+
+    public double subtract(double one, double two) { return one - two; }
+
+    public int multiply(int one, int two) { return one * two; }
+
+    public double multiply(double one, double two) { return one * two; }
+
+    public double divide(int one, double two) { return one / two; }
+
+    public  double divide(double one, double two) { return one / two; }
 
 }
