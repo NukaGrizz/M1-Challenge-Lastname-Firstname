01b5efb8206b6dc2d968e22f6c88984611b47b00
NukaGrizz <nukagrizz@gmail.com>
2022-05-06 00:47:43 -0400

    Corrected naming for Calculator object

---
+++ CalculatorObject/src/main/java/com/company/Calculator.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Calculator {
+
+    public int add(int one, int two) { return one + two; }
+
+    public double add(double one, double two) { return one + two; }
+
+    public int subtract(int one, int two) { return one - two; }
+
+    public double subtract(double one, double two) { return one - two; }
+
+    public int multiply(int one, int two) { return one * two; }
+
+    public double multiply(double one, double two) { return one * two; }
+
+    public int divide(int one, int two) { return one / two; }
+
+    public  double divide(double one, double two) { return one / two; }
+
+}
